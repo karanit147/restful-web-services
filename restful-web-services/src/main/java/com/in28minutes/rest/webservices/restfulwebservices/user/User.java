bbc4f7310a7964fedbef547b59fb28ec1c722bb1
karan <karanit147@gmail.com>
2024-05-07 00:34:38 +0530

    commit 07/05 12.31am - myPC

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -11,8 +13,10 @@ public class User {
 	
 	private Integer id;
 	@Size(min=2, message="Name should have atleast 2 charecters")
+	@JsonProperty("user_name")
 	private String name;
 	@Past(message="Birth date should be in past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;	
 	
 	public User(Integer id, String name, LocalDate birthDate) {
@@ -21,7 +25,7 @@ public class User {
 		this.name = name;
 		this.birthDate = birthDate;
 	}
-	
+	 
 	public Integer getId() {
 		return id;
 	}
